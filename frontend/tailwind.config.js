9b85807d81c40484a5f6feda3b158bf7485b372b
Ara <araceli.rojas123@gmail.com>
2023-11-25 16:56:11 +0100

    pagina_inicio

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,24 @@ export default {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'anybody': ['Anybody', 'sans-serif'],
+      },
       colors: {
         background: '#FFF4EA',
         primary: '#F5CEAF',
         secondary: '#DDBEA9',
-        
-      }
+        'shade-bg': 'rgba(255, 244, 234, 0.8)',
+        'piel-claro': '#fff1e5',
+        'marron':'#543C2E',
+      }, 
+      backgroundImage:{
+        'img-mom': "url('./assets/img-mujer.png')"
+      },
+      backgroundSize:{
+        '30%':'30%',
+        '120%': '120%',
+      }, 
     },
   },
   plugins: [],
