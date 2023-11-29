8ce4f4baa65b40a7e72ca0e11476d798ff4d69d0
R4bits <R4bbits@pm.me>
2023-11-29 15:55:22 +0100

    arreglao

--- frontend/src/components/UserConf.tsx
+++ frontend/src/components/UserConf.tsx
@@ -18,8 +18,13 @@ function UserConf() {
                 </div>
             </nav>
 
+
             <div className='flex flex-col ml-14 text-xl gap-2'>
                 <h1 className='my-10 uppercase text-4xl text-center tracking-wider lg:text-start lg:ml-10'>Ajustes</h1>
+                <label htmlFor="avatar">Avatar
+                    <input type="file" accept='image/*' id='avatar' className='hidden' />
+                    <img src="./src/assets/avatar-person.svg" alt="avatar" className='mb-4 -ml-4 max-w-[5rem]' />
+                </label>
                 <label htmlFor="name">Nombre</label>
                 <span className='text-gray-400 mb-4'>Raquel</span>
                 <label htmlFor="username">Nombre de usuario</label>
