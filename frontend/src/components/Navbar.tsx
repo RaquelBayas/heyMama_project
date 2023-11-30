b63bbdaa8107482677f8bbc2a2bcb27f316bb5af
Ara <araceli.rojas123@gmail.com>
2023-11-30 21:11:42 +0100

    scroll

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,17 +3,50 @@ import { IoHomeOutline } from "react-icons/io5";
 
 function Navbar(){
     return(
-        <nav className='fixed text-anybody text-2xl font-semibold w-full flex flex-row justify-start gap-10 p-4 mt-0 pl-10 border-2 border-solid shadow-md border-b-marron bg-piel-claro'>
-            <IoHomeOutline className='text-3xl hover:scale-125 cursor-pointer transition-transform duration-300 ease-in-out '/>
-            <Link 
-                to= '/about'
-                spy={true} 
-                smooth={true} 
-                offset={50} 
-                duration={500}
-                className='mx-10 tracking-wider transition-transform duration-300 ease-in-out transform cursor-pointer hover:scale-125 uppercase'
-            >Info</Link>
-            <Link to='/contact' className='mx-2 tracking-wider transition-transform duration-300 ease-in-out transform cursor-pointer hover:scale-125 uppercase'>Contacto</Link> 
+        <nav className='fixed font-semibold w-full p-4 mt-0 pl-10 border-2 border-solid shadow-md border-b-marron bg-piel-claro'>
+            <ul className="flex flex-row justify-between gap-5 text-anybody text-2xl uppercase lg:gap-8">
+                <li className='cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out lg:ml-14'>
+                    <a href="/" >
+                        <Link activeClass="active" 
+                            to="landpage" 
+                            spy={true} 
+                            smooth={true} 
+                            offset={0} 
+                            duration={500} 
+                            >
+                                <IoHomeOutline className='mt-0.5 text-3xl' />
+                        </Link>
+                    </a>
+                </li>
+                <div className="flex flex-row lg:mr-72">
+                    <li className='cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out'>
+                        <a href="/">
+                            <Link activeClass="active"
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                                className='mx-10 tracking-wider transition-transform duration-300 ease-in-out transform cursor-pointer hover:scale-125 uppercase'
+                            >
+                                Info
+                            </Link>
+                        </a>
+                    </li>
+                    <li className='cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out'>
+                        <Link
+                        activeClass="active"
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration={500} className='mx-2 tracking-wider transition-transform duration-300 ease-in-out transform cursor-pointer hover:scale-125 uppercase'
+                        >
+                            Contacto
+                        </Link>
+                    </li>
+                </div>
+            </ul> 
         </nav>
     )
 }
