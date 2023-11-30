9f90ecce8b89975acafe178d475877ba2c4347d5
R4bits <R4bbits@pm.me>
2023-11-30 16:38:50 +0100

    Forum design

--- frontend/src/components/Forum.tsx
+++ frontend/src/components/Forum.tsx
@@ -4,23 +4,42 @@ import Search from "./Search"
 function Forum() {
     return (
         <>
-            <header className="bg-primary border-2 border-b-secondary min-h-min py-2">
-                <Search />
-            </header>
-            <div className="flex bg-primary">
+
+            <div className='w-screen h-screen bg-background grid overflow-hidden grid-cols-[100px,1fr]'>
                 <Menu />
-                <main className="flex justify-center">
-                    <h1>¡Bienvenida, Sara!</h1>
-                    <section className="flex flex-col gap-4">
-                        <article>
-                            <h2>Foro</h2>
-                            <p>Comparte y resuelve tus dudas y pensamientos con personas como tú</p>
-                        </article>
-                    </section>
+                <div className="grid w-full grid-rows-[5em_1fr]">
+                    <div className='flex flex-col justify-center mt-3 mb-3'>
+                        <div className='flex justify-evenly'><Search /></div>
+                        <div className='w-full mt-2 mb-2 border-b border-secondary'></div>
+                    </div>
+
+                    <main className="flex justify-center gap-4">
+                        <section className="flex flex-col gap-6">
+                            <h1 className="font-bold text-3xl text-[#755543]">¡BIENVENIDA, SARA!</h1>
+                            <article className="flex bg-white rounded-md p-3 border-2 border-primary w-[42rem] drop-shadow-md">
+                                <img src="./src/assets/foros.svg" alt="Una imagen de gente hablando" width='125px' className="mr-12 bg-primary" />
+                                <div className="mr-12 mt-4">
+                                    <h2 className="font-semibold text-xl my-2">Foros</h2>
+                                    <p className="w-[18rem]">Comparte y resuelve tus dudas y pensamientos con personas como tú</p>
+                                </div>
+                            </article>
+                            <article className="flex bg-white rounded-md p-3 border-2 border-primary w-[42rem]">
+                                <img src="./src/assets/foro-info.svg" alt="Una bombilla" width='125px' className="mr-12 bg-orange-100" />
+                                <div className="mr-12 mt-4">
+                                    <h2 className="font-semibold text-xl my-2">Información</h2>
+                                    <p className="w-[18rem]">Encuentra información más ampliada de parte de profesionales.</p>
+                                </div>
+                            </article>
+                        </section>
+                        <section className="w-[450px] flex-grow-0">
+
+                        </section>
 
 
-                </main>
+                    </main>
+                </div>
             </div>
+
         </>
     )
 }
