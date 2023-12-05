558610d8f0182329717617ccdd9070d5126bc824
Raquel Bayas <31921518+RaquelBayas@users.noreply.github.com>
2023-12-05 13:04:49 +0100

    Mood tracker

--- backend/controllers/mood.controller.js
+++ backend/controllers/mood.controller.js
@@ -41,8 +41,8 @@ async function getMood(req, res, next) {
     
    const {user_id} = req.query;
 
-    const [mood] = await sendQuery('SELECT * FROM mood WHERE user_id=?',[user_id]);
-
+    const mood = await sendQuery('SELECT * FROM mood WHERE user_id=?',[user_id]);
+    console.log('MOOD ALL',await sendQuery('SELECT * FROM mood WHERE user_id=?',[user_id]))
     res.send({
         ok: true,
         error: null,
--- backend/schemas/Mood.js
+++ backend/schemas/Mood.js
@@ -1,7 +1,9 @@
 import {z} from 'zod'
 
 const Mood = z.object({
+    mood_id: z.number().optional(),
     user_id: z.number(),
+    mood_date: z.date().optional(),
     mood_type_id: z.number()
 })
 
