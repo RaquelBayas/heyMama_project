2348954b8bc0e31fe9021f1570f0cea5b2692dc9
R4bits <R4bbits@pm.me>
2023-12-06 13:02:17 +0100

    added subforum_id to get depresion

--- backend/controllers/forum/getforumcard.js
+++ backend/controllers/forum/getforumcard.js
@@ -2,7 +2,7 @@ import { sendQuery } from '../../db/connectDB.js'
 
 
 async function getForumCard(req, res, next) {
-    const results = await sendQuery('SELECT * FROM subforum');
+    const results = await sendQuery('SELECT subforum_id, subforum_title, subforum_content, create_time FROM subforum');
 
     res.send({
         ok: true,
