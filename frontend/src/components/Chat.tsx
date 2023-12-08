66b5362efe650ac383b05827fbda3592d80a1dee
R4bits <R4bbits@pm.me>
2023-12-08 12:41:21 +0100

    probando chat engine

---
+++ frontend/src/components/Chat.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+
+
+function Chat() {
+    return (
+        <ChatEngine
+            height='100vh'
+            projectID='59c997f8-a28a-4463-8f74-51fef4da0a37'
+            userName='Samira'
+            userSecret='QWERTY1234'
+        />
+    )
+}
+
+export default Chat
