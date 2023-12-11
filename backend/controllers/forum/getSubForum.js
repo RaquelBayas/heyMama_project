f23ebf543f34e63320348b6ad99c917ce76abb26
R4bits <R4bbits@pm.me>
2023-12-11 10:39:20 +0100

    render depresion forum

--- backend/controllers/forum/getSubForum.js
+++ backend/controllers/forum/getSubForum.js
@@ -4,10 +4,10 @@ import { zodErrorMap } from '../../helpers/zodErrorMap.js'
 
 async function getSubForum(req, res, next) {
     const id = req.query;
-    console.log('req.query:',id)
-    
+    console.log('req.query:', id)
+
     try {
-        const results = await sendQuery('SELECT * FROM subforum WHERE forum_id=?',id);
+        const results = await sendQuery('SELECT * FROM subforum WHERE forum_id=1');
         if (!results) {
             const errors = zodErrorMap(error);
             return res.status(400).send({
@@ -16,7 +16,7 @@ async function getSubForum(req, res, next) {
                 error: errors
             });
         }
-        console.log('results.',results)
+        console.log('results.', results)
         res.send({
             ok: true,
             data: results,
@@ -26,7 +26,7 @@ async function getSubForum(req, res, next) {
         return next(new Error(error.message))
     }
     next();
-    
+
 }
 
-export {getSubForum};
+export { getSubForum };
