ec568ba7afe4c4e1250829d4f86804a71a516cd4
R4bits <R4bbits@pm.me>
2023-12-13 12:58:56 +0100

    userauth

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { signUp, logIn } from "../controllers/user.controller.js";
+import { signUp, logIn, isLogIn } from "../controllers/user.controller.js";
 import { contact_form } from "../controllers/contact_form.js"
 
 const userRoutes = express.Router();
 
 userRoutes.post('/signup', signUp);
 userRoutes.post('/login', logIn);
+userRoutes.post('/isLogIn', isLogIn);
 userRoutes.post('/contact', contact_form);
 
 export { userRoutes };
