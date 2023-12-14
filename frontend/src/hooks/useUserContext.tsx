ee58477063139a0a0d05097f411a47207bb67f6c
Ara Rojas <araceli.rojas123@gmail.com>
2023-12-14 10:06:35 +0100

    logout

--- frontend/src/hooks/useUserContext.tsx
+++ frontend/src/hooks/useUserContext.tsx
@@ -2,7 +2,14 @@ import { useContext } from 'react';
 import { UserContext } from '../context/UserProvider';
 
 function useUserContext () {
-  return useContext(UserContext);  
+  
+  const context = useContext(UserContext); 
+  
+  if (!context) {
+    throw new Error('Tienes que usar este hook dentro del contexto correcto.');
+  }
+
+  return context;
 }
 
 export default useUserContext;
