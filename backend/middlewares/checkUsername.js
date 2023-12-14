044ed237b8ce6fa7fe35d45548e1aada0450724c
Raquel Bayas <31921518+RaquelBayas@users.noreply.github.com>
2023-12-14 13:33:41 +0100

    Middleware checkUsername

--- backend/middlewares/checkUsername.js
+++ backend/middlewares/checkUsername.js
@@ -1,10 +1,11 @@
 import {sendQuery} from '../db/connectDB.js'
 async function checkUsername(req, res, next){
     const {username} = req.body;
+    console.log(req.body)
     try {
-      const user = await sendQuery('SELECT * FROM users WHERE username=?', [username]);
-  
-      if (user) {
+      const user = await sendQuery('SELECT * FROM users WHERE username=?', username);
+      console.log(user)
+      if (user.length > 0) {
         return res.status(400).send({
           message: "Se ha producido un error. Nombre de usuario no disponible.",
         });
