e7bbc6a6fcd6c33625219787d7266304ab9f826d
Ara Rojas <araceli.rojas123@gmail.com>
2023-12-14 13:53:39 +0100

    backend

--- backend/controllers/profileConfig.js
+++ backend/controllers/profileConfig.js
@@ -1,7 +1,7 @@
 import { User } from "../schemas/User.js";
 import { sendQuery } from '../db/connectDB.js';
 
-async function profileConfig(){
+async function profileConfig(req, res){
     const { success, error, data } = User.safeParse(req.body);
     console.log(data);
 
@@ -14,24 +14,42 @@ async function profileConfig(){
         });
     }
 
-    const { username, name, surname, phone } = configData;
+    const { username, name, surname, phone } = data;
+    const { biography } = data;
 
-    const { biography } = biographyData;
-
-    const { userId } = req.params.userId;
+    const { userId } = req.params;
 
     try {
 
         const updateProfileDataQuery = 'UPDATE users SET username = IFNULL(?,username), name = IFNULL(?,name), surname = (?, surname), phone = IFNULL(?,phone) WHERE id = ?';
 
-        const [result] = await sendQuery(updateProfileDataQuery, [username, name, surname, phone, userId]);
+        const [resultProfile] = await sendQuery(updateProfileDataQuery, [username, name, surname, phone, userId]);
 
         const updateBiography = 'UPDATE data_users SET biography = IFNULL(?,biography) WHERE id = ?';
 
-        const []
+        const [resultBio] = await sendQuery(updateBiography, [biography]);
+
+        if (resultProfile.affectedRows > 0 || resultBio.affectedRows > 0) {
+            res.send({
+                ok: true,
+                data: { message: 'Perfil actualizado con éxito' },
+                error: null
+            });
+        } else {
+            res.send({
+                ok: false,
+                data: null,
+                error: { message: 'No se realizaron actualizaciones en el perfil' }
+            });
+        }
         
     } catch (error) {
         console.error(error);
+        res.send({
+            ok: false,
+            data: null,
+            error: { message: 'Error al actualizar el perfil' }
+        });
     }
 
 }
