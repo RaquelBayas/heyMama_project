cefece084a9f56ef12f9d66ab72ee1ff7b739b1a
Raquel Bayas <31921518+RaquelBayas@users.noreply.github.com>
2023-12-14 13:06:35 +0100

    Username en registro

---
+++ backend/middlewares/checkUsername.js
@@ -0,0 +1,20 @@
+import {sendQuery} from '../db/connectDB.js'
+async function checkUsername(req, res, next){
+    const {username} = req.body;
+    try {
+      const user = await sendQuery('SELECT * FROM users WHERE username=?', [username]);
+  
+      if (user) {
+        return res.status(400).send({
+          message: "Se ha producido un error. Nombre de usuario no disponible.",
+        });
+      }
+      next();
+    } catch (error) {
+      return res.status(500).send({
+        message: error.message,
+      });
+    }
+  };
+
+export default checkUsername;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signUp } from "../controllers/user.controller.js";
-import { checkUsername } from "../middlewares/index.js";
+import checkUsername from "../middlewares/checkUsername.js";
 
 const userRoutes = express.Router();
 
