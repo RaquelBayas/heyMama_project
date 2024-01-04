37d2186b6dca685892149def09bf942d230457ce
Raquel Bayas <31921518+RaquelBayas@users.noreply.github.com>
2024-01-04 12:42:45 +0100

    Deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Set the output directory to 'build'
+    outDir: 'dist',
   },
 });
